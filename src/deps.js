bd20352280239b90d30661279328d30ec6e26f3f
handoko <srabutdotcom@gmail.com>
2024-09-20 06:20:48 +0700

    add dep.d.ts link

--- src/deps.js
+++ src/deps.js
@@ -1,2 +1,5 @@
+// deno-lint-ignore-file no-slow-types
+// @ts-self-types="../type/deps.d.ts"
+
 export { concat, uint8array } from "@aicone/byte"
 export { p521 } from 'npm:@noble/curves@1.6.0/p521';
