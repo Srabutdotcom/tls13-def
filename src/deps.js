8908f93825bf0fdb7337e36287087d6cac3d16ec
handoko <srabutdotcom@gmail.com>
2024-10-14 07:54:31 +0700

    revision in deps.js

--- src/deps.js
+++ src/deps.js
@@ -1,6 +1,9 @@
 // deno-lint-ignore-file no-slow-types
 // @ts-self-types="../type/deps.d.ts"
 
-export { concat, uint8array, Byte } from "jsr:@aicone/byte@0.3.4"
+export { concat, uint8array, Byte, 
+   Uint8BE, Uint16BE, Uint24BE, Uint32BE, maxBytes, uint,
+   getUint8, getUint16, getUint24, getUint32
+ } from "jsr:@aicone/byte@0.3.4"
 export { p521 } from 'npm:@noble/curves@1.6.0/p521';
 
